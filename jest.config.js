9668c363087e0836154ce2ee74ccad8fb1790059
soramon0 <karimacc001@gmail.com>
2021-12-11 18:48:18 +0100

    Setup unit testing with jest

---
+++ jest.config.js
@@ -0,0 +1,19 @@
+// jest.config.js
+const nextJest = require("next/jest");
+
+const createJestConfig = nextJest({
+  dir: "./",
+});
+
+const customJestConfig = {
+  roots: ["<rootDir>/tests/"],
+  setupFilesAfterEnv: ["<rootDir>/tests/jest.setup.ts"],
+  testEnvironment: "jsdom",
+  moduleNameMapper: {
+    "^@/pages/(.*)$": "<rootDir>/pages/$1",
+    "^@/src/(.*)$": "<rootDir>/src/$1",
+    "^@/tests/(.*)$": "<rootDir>/tests/$1",
+  },
+};
+
+module.exports = createJestConfig(customJestConfig);
